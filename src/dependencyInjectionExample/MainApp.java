2f49615db5c213fd6b4298b6c8b9009ec794d99f
HNIM <makkena.haritha7@gmail.com>
2017-11-04 20:10:21 -0500

    Dependency  Inj

--- src/dependencyInjectionExample/MainApp.java
+++ src/dependencyInjectionExample/MainApp.java
@@ -4,8 +4,8 @@ public class MainApp {
 	
 	public static void main(String args[])
 	{
-		Vehicle v = new Vehicle(new LargeEngine(600));
-		Vehicle v1 = new Vehicle(new SmallEngine(200));
+		Vehicle v = new Vehicle(new LargeEngine(600), new LargeEngine("BIGG"));
+		Vehicle v1 = new Vehicle(new SmallEngine(200), new SmallEngine("small"));
 		v.running();
 		v1.running();
 	}
---
+++ src/dependencyInjectionExample/Tires.java
@@ -0,0 +1,7 @@
+package dependencyInjectionExample;
+
+public interface Tires {
+	
+	public void TireSize();
+
+}
