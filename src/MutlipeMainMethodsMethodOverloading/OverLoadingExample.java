d7c742cc9be8cd236a5d5811ea02fda514fc3faa
HNIM <makkena.haritha7@gmail.com>
2017-11-19 17:45:48 -0600

    overloading examples

---
+++ src/MutlipeMainMethodsMethodOverloading/OverLoadingExample.java
@@ -0,0 +1,17 @@
+package MutlipeMainMethodsMethodOverloading;
+
+public class OverLoadingExample {
+	public static void main(String args[])
+	{
+		System.out.println(" Main method with String args[]");
+	}
+	public static void main(String args)
+	{
+		System.out.println(" Main method with String args");
+	}
+	public static void main()
+	{
+		System.out.println(" Main method ");
+	}
+
+}
