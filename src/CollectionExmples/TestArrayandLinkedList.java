a730c06841c347f0d2e10c538300a02a99e0c7fd
HNIM <makkena.haritha7@gmail.com>
2017-11-27 21:34:29 -0600

    Linkedlist examples

---
+++ src/CollectionExmples/TestArrayandLinkedList.java
@@ -0,0 +1,24 @@
+package CollectionExmples;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class TestArrayandLinkedList {
+	
+	public static void main(String arggs[])
+	{
+		LinkedList<String> lst = new LinkedList<>();
+		lst.add("Haritha");
+		lst.add("Venny");
+		lst.add("Makkena");
+		ArrayList<String> lst1 = new ArrayList<>();
+		lst1.add("Nimmaturi");
+		lst1.add("Charan");
+		lst1.add("Family");
+		System.out.println("Linked List" +lst);
+		System.out.println("Array List" + lst1);
+		
+	}
+		
+
+}
