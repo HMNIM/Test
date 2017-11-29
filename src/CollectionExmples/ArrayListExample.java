835490c0b5539cf9c5a1f2a11a3472f3f38bd36c
HNIM <makkena.haritha7@gmail.com>
2017-11-28 20:36:33 -0600

    ArraylistExamples

--- src/CollectionExmples/ArrayListExample.java
+++ src/CollectionExmples/ArrayListExample.java
@@ -1,11 +1,24 @@
 package CollectionExmples;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class ArrayListExample {
 	public static void main(String args[])
 	{
-		ArrayList<String> obj = new ArrayList<>();
+		ArrayList<String> obj1 = new ArrayList<String>(
+				Arrays.asList("one","two","three","four"));
+		System.out.println("Numbers are" +obj1);
+		
+		ArrayList<Integer> a1 = new ArrayList<Integer>(
+				Arrays.asList(1,2,3,4,5));
+		System.out.println("Numbers" +a1);
+		
+	ArrayList<Integer> intList = new ArrayList<Integer>(Collections.nCopies(10, 5));
+	System.out.println(" Collections are" +intList) ;			
+		ArrayList<String> obj = new ArrayList<String>();
 		obj.add("C");
 		obj.add("D");
 		obj.add("E");
@@ -17,6 +30,7 @@ public class ArrayListExample {
 		// Removing elements from Index
 		obj.remove("C");
 		obj.remove(3);
+		obj.set(2, "Z");
 		System.out.println("Removing elements" +obj);
 	}
 
