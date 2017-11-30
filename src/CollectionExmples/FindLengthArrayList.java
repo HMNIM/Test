35ae68948e4c9e93f9ea3191268a10a0daafa5c5
HNIM <makkena.haritha7@gmail.com>
2017-11-29 21:49:37 -0600

    array list size

--- src/CollectionExmples/FindLengthArrayList.java
+++ src/CollectionExmples/FindLengthArrayList.java
@@ -1,9 +1,6 @@
 package CollectionExmples;
-
 import java.util.ArrayList;
-
 public class FindLengthArrayList {
-
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		System.out.println("Starting or Initial size " +list.size());
