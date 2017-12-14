b4ab3f341d93bed4d3448ab02129a4655cce8a93
HNIM <makkena.haritha7@gmail.com>
2017-12-13 19:40:42 -0600

    Thread

--- src/multiThreading/TestCallRun.java
+++ src/multiThreading/TestCallRun.java
@@ -1,30 +1,20 @@
 package multiThreading;
 
-<<<<<<< HEAD
-public class TestCallRun {
-	public void run()
-	{
-		System.out.println("Running");
-=======
+
 public class TestCallRun extends Thread {
 
 	public void run()
 	{
 		System.out.println("running");
->>>>>>> 4a58bdf16cd3657476966618bd826cf8ddd8a475
+
 	}
 	public static void main(String[] args) {
 		
 		TestCallRun t = new TestCallRun();
 		t.run();
-<<<<<<< HEAD
-		
-		
-=======
-	
-		
 
->>>>>>> 4a58bdf16cd3657476966618bd826cf8ddd8a475
+	
+
 	}
 
 }
