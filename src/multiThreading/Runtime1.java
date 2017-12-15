b282d75108212676bb0846240a9c8d78d68c227d
HNIM <makkena.haritha7@gmail.com>
2017-12-14 22:06:14 -0600

    Runtime

---
+++ src/multiThreading/Runtime1.java
@@ -0,0 +1,19 @@
+package multiThreading;
+
+import java.io.IOException;
+
+public class Runtime1 {
+
+	public static void main(String[] args) {
+
+		try {
+			Runtime.getRuntime().exec("notepad");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(Runtime.getRuntime().availableProcessors());
+	}
+
+}
